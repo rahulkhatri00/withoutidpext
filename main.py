6fad054fc34204a865bc11f326725869dff2ee95
rahulkhatri00 <116899776+rahulkhatri00@users.noreply.github.com>
2025-05-07 20:20:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -41,9 +41,9 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 
 # Bot credentials from environment variables (Render compatible)
-API_ID = int(os.environ.get("API_ID", 24473318))
-API_HASH = os.environ.get("API_HASH", "e7dd0576c5ac0ff8f90971d6bb04c8f5")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8034069514:AAHUBpzSCq41jPwsJkDbXuEoVC_yCxzyuw0")
+API_ID = int(os.environ.get("API_ID", 27355149))
+API_HASH = os.environ.get("API_HASH", "68bcb1ab22bd80bbae9d66ccddf540c2")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8095251181:AAHU2Ik7XQ0PnpZQo2eBoMoGY63gKvIOVkQ")
 
 # Initialize Bot Globally (IMPORTANT FIX)
 bot = Client("bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
@@ -56,7 +56,7 @@ def home():
     return "Bot is running!"
 
 def run_flask():
-    app.run(host="0.0.0.0", port=1000) #Use 8080 Port here, if you're deploying it on koyeb
+    app.run(host="0.0.0.0", port=8080) #Use 8080 Port here, if you're deploying it on koyeb
     
 
 image_list = [
